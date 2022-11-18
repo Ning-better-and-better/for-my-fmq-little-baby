61e1823612e4f72b5188d7bc1476d5261f9badf4
Ning Guo <117777929+Ning-better-and-better@users.noreply.github.com>
2022-11-18 13:06:11 -0500

    Update plot_data.m

--- plot_data.m
+++ plot_data.m
@@ -1,6 +1,8 @@
 
 
-%% read data file (make sure they are in the same folder)
+%% read data file
+% make sure they are in the same folder
+% do not skip any fig setting
 clear all;clc;
 load('data.mat')
 
@@ -29,9 +31,9 @@ grid on
 xlim([W(1,index),W(end,index)])
 
 % save the data
-wavelength = W(:,index);
-intensity = I(:,index);
-save('fig2','wavelength','intensity')
+%wavelength = W(:,index)';
+%intensity = I(:,index)';
+%save('fig2','wavelength','intensity')
 
 %% figure 3 -- the index(x-axis) of the wavelength you may concern
 figure(3)
@@ -41,12 +43,22 @@ grid on
 %% figure 4 -- for a certain wavelength, X,Y vs I
 % you need to check the index(x-axis) of the waveform you want to check
 figure(4)
-index = 500;
-plot3(X(index,:),Y(index,:),I(index,:),'o')
+index = 2792;
+
+j = 1; k = 1;
+for i = 1:length(X(index,:))
+    x(j,k) = X(index,i);
+    y(j,k) = Y(index,i);
+    inten(j,k) = I(index,i);
+    j = j + 1;
+    if mod(j,col_num+1) == 0
+        j = 1;
+        k = k + 1;
+    end
+end
+s = surf(x,y,inten)
+s.EdgeColor = 'none';
 grid on
 box on
 % save the data
-x = X(index,:);
-y = Y(index,:);
-intensity = I(index,:);
-save('fig4','x','y','intensity')
+%save('fig4','x','y','intensity')
